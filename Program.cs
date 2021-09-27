a625ed43a3afed43fe1e2c04258e27660cd97191
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-27 23:03:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gen_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>();
+            st.Push(88);
+            st.Push(33);
+            st.Push(45);
+            st.Push(56);
+            st.Push(44);
+            foreach(int ob in st)
+            {
+                Console.Write(ob+" ");
+            }
+            Console.WriteLine("\nRemove an element");
+            st.Pop();
+            foreach (int ob in st)
+            {
+                Console.Write(ob+" ");
+            }
+            
+        }
+       
+    }
+}
